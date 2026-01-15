6edb2a1f4d5c7a37054b01cd0c1eb4dd1fe7330a
Simolinus <65534780+Simolinus@users.noreply.github.com>
2026-01-15 13:51:51 +0200

    module2

---
+++ module2.py
@@ -0,0 +1,7 @@
+name = input("Enter name: ")
+print("Hello, "+name+"!")
+ 
+print("Enter the radius of a circle")
+number = input("Radius: ")
+areanumber = number**2
+print("Area of circle is equal to: "+areanumber)
