a7e64db56cc10c4cd4891edfb7e2fc110696ff84
Simolinus <65534780+Simolinus@users.noreply.github.com>
2026-01-15 14:58:10 +0200

    module2 task 1 and 2 and ignore IDE config

--- module2.py
+++ module2.py
@@ -1,7 +1,10 @@
+import math
 name = input("Enter name: ")
 print("Hello, "+name+"!")
  
 print("Enter the radius of a circle")
 number = input("Radius: ")
-areanumber = number**2
-print("Area of circle is equal to: "+areanumber)
+areanumber = math.pi*float(number)**2
+# print("Area of circle is equal to: "+str(areanumber))
+print(f"Area of circle is equal to: {areanumber:.3f}")
+
